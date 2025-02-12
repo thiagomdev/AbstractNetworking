72ab315f2780d6cca633c46c0be51935bf7af99e
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2025-02-12 20:48:52 -0300

    - Removing token

--- HTTPClient/Feature/Scenes/Main/Service/MainService.swift
+++ HTTPClient/Feature/Scenes/Main/Service/MainService.swift
@@ -25,11 +25,7 @@ extension MainService: MainServicing {
         if let url = URL(string: strUrl) {
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
-            request.allHTTPHeaderFields = [
-                "accept": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjk0NDk4ZDA0ZGFjZDFjZjk2NjQ4YmIxN2NlYmM2NyIsIm5iZiI6MTY5MzQwMDczNS44NjA5OTk4LCJzdWIiOiI2NGVmM2U5Zjk3YTRlNjAwYzQ4NjJjZGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.AAAJG3OLN5csc3p4V0MJyNwmMpGbPJcIIU-SwYWDrv8"
-            ]
-            
+
             client.execute(request) { result in
                 switch result {
                 case let .success(data, httpResponse):
